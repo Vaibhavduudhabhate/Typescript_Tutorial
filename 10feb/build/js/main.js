a55bba224ac6677183e5357e1bf23f931e8d95ad
Vaibhavduudhabhate <dudhabhatevaibhav@gmail.com>
2025-02-12 17:11:27 +0530

    10feb 2

--- 10feb/build/js/main.js
+++ 10feb/build/js/main.js
@@ -1,24 +1,81 @@
 "use strict";
-// Type assertions and type casting
-// convert to more or less specific
-let a = 'hello';
-let b = a;
-let c = a;
-let d = 'world';
-let e = 'world';
-const addOrConcat = (a, b, c) => {
-    if (c === 'add')
-        return a + b;
-    return '' + a + b;
-};
-let myVal = addOrConcat(2, 2, 'concat');
-// be careful in such case ts has no problem but it will return a string;
-let nextVal = addOrConcat(2, 2, 'concat');
-// 10 as string;
-10; // this is called double casting
-// The DOM
-const img = document.querySelector('img');
-const MyImg = document.getElementById('img');
-const nextImg = document.getElementById('img');
-// img?.src
-// MyImg.src
+class coder {
+    constructor(name, music, age, lang = "Typescript") {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
+    getAge() {
+        return `hello, I'm ${this.age}`;
+    }
+}
+const Dave = new coder('Dave', 'Rock', 42);
+console.log(Dave.getAge());
+// console.log(Dave.age)
+// console.log(Dave.lang)
+class webDev extends coder {
+    constructor(computer, name, music, age) {
+        super(name, music, age);
+        this.computer = computer;
+        this.computer = computer;
+    }
+    getLang() {
+        return `I write ${this.lang}`;
+    }
+}
+const Sara = new webDev('Mac', "Sara", 'Lofi', 25);
+console.log(Sara.getLang());
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} ${this.instrument}`;
+    }
+}
+const Page = new Guitarist("Jimmy", "guitar");
+console.log(Page.play('strums'));
+////////////////////////////////////////////////
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0;
+const John = new Peeps('John');
+const Steve = new Peeps('Steve');
+const Amy = new Peeps('Amy');
+console.log(Peeps.count);
+////////////////////////////////////
+class Bands {
+    constructor() {
+        this.datastate = [];
+    }
+    get data() {
+        return this.datastate;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every(el => typeof el === "string")) {
+            this.datastate = value;
+        }
+        else
+            throw new Error('Param is not an array of strings');
+    }
+}
+const MyBands = new Bands();
+MyBands.data = ['Neil Young', 'Led Zep'];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, 'zz top'];
+console.log(MyBands.data);
+// MyBands.data = ['Van Halen', 5150]
--- 10feb/src/main.ts
+++ 10feb/src/main.ts
@@ -6,11 +6,118 @@ class coder{
     secondLang! : string
 
     constructor(
-       public readonly name:string,public music:string,private age:number,protected lang:string
+       public readonly name:string,
+       public music:string,
+       private age:number,
+       protected lang:string = "Typescript"
     ){
         this.name = name
         this.music = music
         this.age = age
         this.lang = lang
     }
+
+    public getAge(){
+        return `hello, I'm ${this.age}`
+    }
 }
+
+const Dave = new coder('Dave','Rock',42 )   
+console.log(Dave.getAge())
+// console.log(Dave.age)
+// console.log(Dave.lang)
+
+class webDev extends coder{
+    constructor(public computer: string,
+        name: string,
+        music: string,
+        age:number){
+        super(name,music,age)
+        this.computer = computer
+    }
+
+    public getLang(){
+        return `I write ${this.lang}`
+    }
+}
+
+const Sara = new webDev('Mac',"Sara",'Lofi',25)
+
+console.log(Sara.getLang())
+// console.log(Sara.age);
+// console.log(Sara.lang);
+
+////////////////////////////////////////////////
+
+interface Musician{
+    name:string,
+    instrument:string,
+    play(action: string): string
+}
+
+class Guitarist implements Musician{
+    name: string
+    instrument: string
+
+    constructor(name:string ,instrument:string){
+        this.name = name
+        this.instrument = instrument
+    }
+
+    play(action: string) {
+        return `${this.name} ${action} ${this.instrument}`
+    }
+}
+
+const Page =  new Guitarist("Jimmy","guitar")
+console.log(Page.play('strums'))
+////////////////////////////////////////////////
+
+class Peeps{
+    static count: number = 0
+
+    static getCount(): number{
+        return Peeps.count
+    }
+
+    public id :number
+
+    constructor(public name : string){
+        this.name = name
+        this.id = ++Peeps.count    
+    }
+}
+
+const John = new Peeps('John')
+const Steve = new Peeps('Steve')
+const Amy = new Peeps('Amy')
+
+console.log(Peeps.count)
+
+////////////////////////////////////
+
+class Bands {
+    private datastate :string[]
+
+    constructor(){
+        this.datastate = []
+    }
+
+    public get data(): string[]{
+        return this.datastate
+    }
+
+    public set data(value :string[]){
+        if (Array.isArray(value) && value.every(el => typeof el === "string")) {
+            this.datastate = value
+        }else throw new Error('Param is not an array of strings')
+    }
+}
+
+const MyBands = new Bands()
+
+MyBands.data = ['Neil Young' , 'Led Zep']
+console.log(MyBands.data)
+MyBands.data = [...MyBands.data,'zz top']
+console.log(MyBands.data)
+// MyBands.data = ['Van Halen', 5150]
