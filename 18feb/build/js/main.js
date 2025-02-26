eec2285a8112d3b405891db8c5cd228c56092d66
Vaibhavduudhabhate <dudhabhatevaibhav@gmail.com>
2025-02-26 10:45:34 +0530

    typesript first project in react

---
+++ 18feb/build/js/main.js
@@ -0,0 +1,19 @@
+"use strict";
+const echo = (arg) => arg;
+const isObj = (arg) => {
+    return (typeof arg === 'object' && !Array.isArray(arg) && arg !== null);
+};
+console.log(isObj(true));
+console.log(isObj('john'));
+console.log(isObj([1, 2, 3]));
+console.log(isObj({ name: "John" }));
+console.log(isObj(null));
+const isTrue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false };
+    }
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { arg, is: false };
+    }
+    return { arg, is: !!arg };
+};
