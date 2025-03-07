94823342e5501e9c9b7b79a1c9a46d6a7491cdf1
Vaibhavduudhabhate <dudhabhatevaibhav@gmail.com>
2025-03-07 12:10:05 +0530

    router example typescript

--- MovieDbTypescript/25feb/src/App.tsx
+++ MovieDbTypescript/25feb/src/App.tsx
@@ -1,67 +1,57 @@
-import { useState } from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route ,NavLink} from "react-router-dom";
+import { Home } from './Pages/Home';
+import { About } from './Pages/About';
+import Contact from './Pages/Contact';
+import { IoIosHome  } from "react-icons/io";
+import { AiOutlineGlobal } from 'react-icons/ai';
+import { RiContactsBook3Line } from 'react-icons/ri';
 function App() {
-  const [count, setCount] = useState(0);
-  const [namearray , setName] = useState<string[]>([]);
-
-  const genarateRandomString = () =>{
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
-    const length = 5;
-    for (let i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return result;  
-  }
-
-  function handleAddRandomString() {
-    const randomString = genarateRandomString();
-    setName((prevArray) => [...prevArray, randomString]);
-  }
-
-  const handleRemoveRandomString = () => {
-    setName((prevArray) => prevArray.slice(0,-1));
-  };
 
 
   return (
-    <>
-      <div className="card flex items-center justify-center py-[20px] bg-gray-600 min-h-[100vh] h-[100%]">
-        <div className='text-xl w-[500px] flex flex-col gap-[10px] text-center'>
-          <p className='border h-[60px] flex items-center justify-center text-white'>{count}</p>
-          <button onClick={() => setCount(count + 1)}>
-            Increment button
-          </button>
-          <button onClick={() => setCount(count - 1)}>
-            Decrement button
-          </button><br />
-          <button onClick={()=> setCount(count + 2)}>
-            Increment By 2
-          </button>
-          <button onClick={()=> setCount(count - 2)}>
-            Decrement By 2
-          </button><br />
+    <Router>
+    <nav>
+  <ul className="flex gap-[20px] justify-center items-center py-[30px] text-[20px] bg-gray-900">
+    <li>
+      <NavLink 
+        to="/" 
+        className={({ isActive }) => 
+          `flex flex-row gap-1 items-center px-4 py-2 rounded ${isActive ? "bg-green-300 text-black" : "text-gray-700 bg-gray-200"}`
+        }
+      >
+        Home<IoIosHome />
+      </NavLink>
+    </li>
+    <li>
+      <NavLink 
+        to="/about" 
+        className={({ isActive }) => 
+          `flex flex-row gap-1 items-center px-4 py-2 rounded ${isActive ? "bg-green-300 text-black" : "text-gray-700 bg-gray-200"}`
+        }
+      >
+        About<AiOutlineGlobal />
+      </NavLink>
+    </li>
+    <li>
+      <NavLink 
+        to="/contact" 
+        className={({ isActive }) => 
+          `flex flex-row gap-1 items-center px-4 py-2 rounded ${isActive ? "bg-green-300 text-black" : "text-gray-700 bg-gray-200"}`
+        }
+      >
+        Contact<RiContactsBook3Line />
+      </NavLink>
+    </li>
+  </ul>
+</nav>
 
-{/* Dummy Comment */}
-          <button onClick={handleAddRandomString}>
-            Add Random String to Array
-          </button>
-
-          <div className='border h-[100%] py-[10px] flex flex-col items-center justify-center text-white'> 
-            <h3>Random Strings in Array:</h3>
-            <ul>
-              {namearray.map((name, index) => (
-                <li key={index}>{name}</li>
-              ))}
-            </ul>
-          </div>
-          <button onClick={handleRemoveRandomString}>
-              remove last String from Array
-          </button>
-        </div>
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </Router>
   );
 }
 
---
+++ MovieDbTypescript/25feb/src/Pages/Home.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+export const Home = () => {
+    const [count, setCount] = useState(0);
+    const [namearray , setName] = useState<string[]>([]);
+  
+    const genarateRandomString = () =>{
+      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      let result = '';
+      const length = 5;
+      for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+      }
+      return result;  
+    }
+  
+    function handleAddRandomString() {
+      const randomString = genarateRandomString();
+      setName((prevArray) => [...prevArray, randomString]);
+    }
+  
+    const handleRemoveRandomString = () => {
+      setName((prevArray) => prevArray.slice(0,-1));
+    };
+  
+  return (
+        <>
+      <div className="card flex items-center justify-center py-[20px] bg-gray-600 min-h-[100vh] h-[100%]">
+        <div className='text-xl w-[500px] flex flex-col gap-[10px] text-center'>
+          <p className='border h-[60px] flex items-center justify-center text-white'>{count}</p>
+          <button onClick={() => setCount(count + 1)}>
+            Increment button
+          </button>
+          <button onClick={() => setCount(count - 1)}>
+            Decrement button
+          </button><br />
+          <button onClick={()=> setCount(count + 2)}>
+            Increment By 2
+          </button>
+          <button onClick={()=> setCount(count - 2)}>
+            Decrement By 2
+          </button><br />
+
+          <button onClick={handleAddRandomString}>
+            Add Random String to Array
+          </button>
+
+          <div className='border h-[100%] py-[10px] flex flex-col items-center justify-center text-white'> 
+            <h3>Random Strings in Array:</h3>
+            <ul>
+              {namearray.map((name, index) => (
+                <li key={index}>{name}</li>
+              ))}
+            </ul>
+          </div>
+          <button onClick={handleRemoveRandomString}>
+              remove last String from Array
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
