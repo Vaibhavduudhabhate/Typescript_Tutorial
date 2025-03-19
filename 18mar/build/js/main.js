7bdfe047c7d48a54bbd1ffb0b931d681217819f2
Vaibhavduudhabhate <dudhabhatevaibhav@gmail.com>
2025-03-19 16:47:32 +0530

    19march

--- 18mar/build/js/main.js
+++ 18mar/build/js/main.js
@@ -1,3 +1,13 @@
 "use strict";
 const numbers = [1, 2, 3, 4, 5];
-console.log(numbers);
+// ! console.log(numbers)
+// !--------------------------------------------------------
+// ? double the all numbers in  array
+const DoubleData = numbers.map((currVal) => currVal * 2);
+console.log(DoubleData);
+// !--------------------------------------------------------
+// ? Converting  Number to string
+const numberToString = numbers.map((currElm) => currElm.toString());
+console.log(numberToString);
+// !--------------------------------------------------------
+// todo
--- 18mar/src/main.ts
+++ 18mar/src/main.ts
@@ -1,2 +1,19 @@
 const numbers : number[] = [1,2,3,4,5];
-console.log(numbers)
+// ! console.log(numbers)
+
+// !--------------------------------------------------------
+
+// ? double the all numbers in  array
+const DoubleData:number[] = numbers.map((currVal)=> currVal * 2)
+console.log(DoubleData)
+
+// !--------------------------------------------------------
+
+// ? Converting  Number to string
+
+const numberToString = numbers.map((currElm: number)=> currElm.toString())
+console.log(numberToString)
+
+// !--------------------------------------------------------
+
+// todo
